b6436d5976c2bdc68b1100e0613c3b630c20e67b
fumiya <buchi0326iwa@gmail.com>
2022-03-07 21:19:42 +0900

    コレクションビューの導入

--- custom-app/custom-app/ViewController.swift
+++ custom-app/custom-app/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    /**
+     ビュー描画終了時
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        habitationCollectionView.delegate = self
+        habitationCollectionView.dataSource = self
     }
+    
+    @IBOutlet weak var habitationCollectionView: UICollectionView!
+    // 仮のデータ
+    var TODO: [String] = ["a", "b", "c"]
+    
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "habitationCell", for: indexPath) as! HabitationCollectionViewCell
+        cell.dueTimeLabel.text = "6:00"
+        cell.titleLable.text = TODO[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return TODO.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, canEditItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let tmpTODO = TODO.remove(at: sourceIndexPath.row)
+        TODO.insert(tmpTODO, at: destinationIndexPath.row)
+    }
+    
+    
 
 }
 
